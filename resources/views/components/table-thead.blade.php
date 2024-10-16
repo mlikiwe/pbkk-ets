6265115d93b1f938f1f3957df268c261fd6361e5
mlikiwe <adinataivan326@gmail.com>
2024-10-17 00:01:51 +0700

    fix: color table header

--- resources/views/components/table-thead.blade.php
+++ resources/views/components/table-thead.blade.php
@@ -1,3 +1,3 @@
-<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+<thead class="text-xs text-white uppercase bg-blue-700 dark:bg-gray-700 dark:text-gray-400">
     {{ $slot }}
 </thead>
